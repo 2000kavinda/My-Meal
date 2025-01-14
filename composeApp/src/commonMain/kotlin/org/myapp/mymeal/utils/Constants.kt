22b188a6e76c774d5ae4d704ecefc07e52ecbc01
2000kavinda <98000159+2000kavinda@users.noreply.github.com>
2025-01-14 23:29:53 +0530

    updated

--- composeApp/src/commonMain/kotlin/org/myapp/mymeal/utils/Constants.kt
+++ composeApp/src/commonMain/kotlin/org/myapp/mymeal/utils/Constants.kt
@@ -1,8 +1,8 @@
 package org.myapp.mymeal.utils
 
 object Constants {
-    const val keyValue = "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"
-    const val tokenValue = "07034037979061f548f175ff21f1a5db734182e9"
+    const val keyValue = "sk-proj-ol3VJytWAEJXgsa5qdKxI6_0J63048848Oa3SqNskTBqLSJMC2eiG6zPUPUr_qHlnQebHvXU2kUHj8CT3BlbkFJMXq8Oz5vfTBEt7mXvjQcEtdFDCh_aaVlkHZTIpf1M2HwadQkLvadoJCWX4QPICGXv9z5yZkqgA"
+    const val tokenValue = "07034037979061f548f175ff21f1a5db734182e915155"
 
     const val logoUrl = "https://firebasestorage.googleapis.com/v0/b/care-cost.appspot.com/o/meal%20photos%2FUntitled_design__1_-removebg.png?alt=media&token=0dacbe0d-7fa8-407a-86ab-020832fb83b8"
     const val gameBgUrl = "https://firebasestorage.googleapis.com/v0/b/care-cost.appspot.com/o/gifs%2FDALL%C2%B7E%202024-12-11%2003.25.03%20-%20A%20vibrant%2C%20cartoon-style%20background%20showing%20train%20tracks%20in%20a%20city%20setting%20from%20a%20bottom-to-up%20perspective.%20The%20scene%20includes%20detailed%20graffiti%20art%20o.webp?alt=media&token=76fedd2e-8533-4a06-b6a1-427819a5b9f1"
